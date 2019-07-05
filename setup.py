894c01175ad097c5a975331c7337ac2fe7045811
cviejom <carlosviejom@hotmail.com>
2019-07-05 17:40:34 -0600

    chore: Update all the project structure
    Structure all the project folder structure based on the machine learning
    Cookiecutter Data Science project
    
    No issue tracking associated with this commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project, consists on building a stock price predictor',
+    author='cviejom',
+    license='MIT',
+)
